feda6cedfd278cbd1c4deb0e984db0542b6c2efa
Racekid16 <1750801177a@gmail.com>
2026-07-24 22:00:38 -0400

    Rename files in snake case

--- 1.contains_duplicate.py
+++ 1.contains_duplicate.py
@@ -1,16 +1,18 @@
-# Run with: python3 1.ContainsDuplicate.py 1.ContainsDuplicateTests.json
+# Run with: python3 1.contains_duplicate.py 1.contains_duplicate_tests.json
 
 import sys
 import json
 from typing import List
 
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"usage: {sys.argv[0]} <test_cases.json>")
+        print(f"usage: python3 {sys.argv[0]} <test_cases.json>")
         sys.exit(1)
 
     with open(sys.argv[1]) as f:
@@ -20,4 +22,3 @@ if __name__ == "__main__":
     for case in test_cases:
         result = sol.hasDuplicate(case)
         print(f"{case} -> {result}")
-
